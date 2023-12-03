212fc3d1ce060e50306700b31c5bf9c190fbae5f
alberinameshi <152774261+alberinameshi@users.noreply.github.com>
2023-12-03 09:57:20 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const apiBaseUrl = 'https://perfect-ruby-suit.cyclic.app'; 
+const apiBaseUrl = 'https://drab-blue-cod-garb.cyclic.app'; 
 const token = "6437257115:AAFDWT1bCe7lJ_mVVQvY6tD75vSF1hs03uw";
 const chatId = "-1002124888543";
 export default {
